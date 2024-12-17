004c48057acf26c09b19d82f4f9ea7bbc1251e68
a3ternus <luanvitorlima2017@gmail.com>
2024-12-17 14:46:46 -0300

    sistema de cadastro

--- AppGestao/InterfaceProject.Designer.cs
+++ AppGestao/InterfaceProject.Designer.cs
@@ -28,21 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InterfaceProject));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.serviceBox = new System.Windows.Forms.ComboBox();
+            this.radiusNao = new System.Windows.Forms.RadioButton();
+            this.radiusSim = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Interface
+            // groupBox1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.groupBox1.Controls.Add(this.btnClean);
+            this.groupBox1.Controls.Add(this.serviceBox);
+            this.groupBox1.Controls.Add(this.radiusNao);
+            this.groupBox1.Controls.Add(this.radiusSim);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.btnInsert);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Controls.Add(this.label1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // btnClean
+            // 
+            resources.ApplyResources(this.btnClean, "btnClean");
+            this.btnClean.Name = "btnClean";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // serviceBox
+            // 
+            this.serviceBox.FormattingEnabled = true;
+            resources.ApplyResources(this.serviceBox, "serviceBox");
+            this.serviceBox.Name = "serviceBox";
+            // 
+            // radiusNao
+            // 
+            resources.ApplyResources(this.radiusNao, "radiusNao");
+            this.radiusNao.Checked = true;
+            this.radiusNao.Name = "radiusNao";
+            this.radiusNao.TabStop = true;
+            this.radiusNao.UseVisualStyleBackColor = true;
+            // 
+            // radiusSim
+            // 
+            resources.ApplyResources(this.radiusSim, "radiusSim");
+            this.radiusSim.Name = "radiusSim";
+            this.radiusSim.TabStop = true;
+            this.radiusSim.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnInsert
+            // 
+            resources.ApplyResources(this.btnInsert, "btnInsert");
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // dateTimePicker1
+            // 
+            resources.ApplyResources(this.dateTimePicker1, "dateTimePicker1");
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // txtNome
+            // 
+            resources.ApplyResources(this.txtNome, "txtNome");
+            this.txtNome.Name = "txtNome";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // lista
+            // 
+            this.lista.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lista.FormattingEnabled = true;
+            resources.ApplyResources(this.lista, "lista");
+            this.lista.Name = "lista";
+            // 
+            // InterfaceProject
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Menu;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Name = "Interface";
-            this.Text = "Interface";
+            this.MaximizeBox = false;
+            this.Name = "InterfaceProject";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radiusNao;
+        private System.Windows.Forms.RadioButton radiusSim;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.ComboBox serviceBox;
+        private System.Windows.Forms.Button btnClean;
     }
 }
--- AppGestao/InterfaceProject.cs
+++ AppGestao/InterfaceProject.cs
@@ -1,12 +1,101 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace AppGestao
 {
     public partial class InterfaceProject : Form
     {
+        List<Cadastro> servico;
         public InterfaceProject()
         {
             InitializeComponent();
+            servico = new List<Cadastro>();
+
+            serviceBox.Items.Add("Ilustração Digital");
+            serviceBox.Items.Add("Emoticons");
+            serviceBox.Items.Add("Character Design");
+            serviceBox.Items.Add("Arte para Games");
+
+            serviceBox.SelectedIndex = 0;
         }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            int index = -1;
+
+            foreach (Cadastro cadastro in servico)
+            {
+                if (cadastro.Nome == txtNome.Text)
+                {
+                    index = servico.IndexOf(cadastro);
+                }
+            }
+
+            if (txtNome.Text == "")
+            {
+                MessageBox.Show("Preencha o Campo Nome",
+                                    "A3TERNUS - Gestão de Clientes",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Warning);
+                txtNome.Focus();
+                return;
+            }
+
+            if (serviceBox.Text == "")
+            {
+                MessageBox.Show("Preencha o Campo de Serviço",
+                                    "A3TERNUS - Gestão de Clientes",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Warning);
+                serviceBox.Focus();
+                return;
+            }
+
+            char finalizado;
+
+            if (radiusSim.Checked)
+            {
+                finalizado = 'S';
+            }
+            else
+            {
+                finalizado = 'N';
+            }
+
+            Cadastro cadastrar = new Cadastro();
+
+            cadastrar.Nome = txtNome.Text;
+            cadastrar.Servico = serviceBox.Text;
+
+            if (index < 0)
+            {
+                servico.Add(cadastrar);
+            }
+            else
+            {
+                servico[index] = cadastrar;
+            }
+
+            btnClean_Click(btnClean, EventArgs.Empty);
+
+            Listar();
+        }
+
+        private void Listar()
+        {
+            lista.Items.Clear();
+
+            foreach (Cadastro cadastro in servico)
+            {
+                lista.Items.Add(cadastro.Nome + " " + cadastro.Servico);
+            }
+        }
+
+        private void btnClean_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
     }
 }
